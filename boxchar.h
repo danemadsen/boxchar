f7ee65524c6d4059ffe3130c3ca9b8c143eb7fbb
danemadsen <dane_madsen@hotmail.com>
2023-09-04 19:39:59 +1000

    Initial

--- boxchar.h
+++ boxchar.h
@@ -1,5 +1,14 @@
 /*
-    Boxchar is a simple header only library for creating text based user interfaces in C.
+                ######  ####### #     #  #####  #     #    #    ######  
+                #     # #     #  #   #  #     # #     #   # #   #     # 
+                #     # #     #   # #   #       #     #  #   #  #     # 
+                ######  #     #    #    #       ####### #     # ######  
+                #     # #     #   # #   #       #     # ####### #   #   
+                #     # #     #  #   #  #     # #     # #     # #    #  
+                ######  ####### #     #  #####  #     # #     # #     #
+    
+    Boxchar is a simple header only library for creating text based user interfaces 
+    in C and C++. It is designed to be lightweight and easy to use.
 
     MIT License
 
